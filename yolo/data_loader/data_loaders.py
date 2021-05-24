5053f87da1db558e9a11f63400fd6f94dcc8d258
ngocgiang99 <truongngocgiang99@gmail.com>
2021-05-24 18:10:41 +0700

    fix bug dataloader

--- yolo/data_loader/data_loaders.py
+++ yolo/data_loader/data_loaders.py
@@ -3,6 +3,7 @@ from base import BaseDataLoader
 
 
 from torchvision.transforms import ToTensor
+import torch
 
 class MnistDataLoader(BaseDataLoader):
     """
@@ -30,20 +31,75 @@ class VocDataLoader(BaseDataLoader):
             # transforms.ToTensor(),
         ])
 
-        # target_tranform = transforms.Compose([
-        #     transforms.ToTensor(),
-        # ])
+        target_tranform = VocDataLoader.anotations_tranform
         self.data_dir = data_dir
-        self.dataset = datasets.VOCDetection(self.data_dir, image_set="trainval", download=False, transform=trsfm)
+        self.dataset = datasets.VOCDetection(self.data_dir, image_set="trainval", download=False, transform=trsfm, target_transform=target_tranform)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+    def anotations_tranform(x):
+        labels_map = {  'aeroplane' : 1,
+                        'bicycle' : 2,
+                        'bird' : 3, 
+                        'boat' : 4,
+                        'bottle' : 5,
+                        'bus' : 6,
+                        'car' : 7,
+                        'cat' : 8,
+                        'chair' : 9,
+                        'cow' : 10,
+                        'diningtable' : 11,
+                        'dog' : 12,
+                        'horse' : 13,
+                        'motorbike' : 14,
+                        'person' : 15,
+                        'pottedplant' : 16,
+                        'sheep': 17, 
+                        'sofa': 18, 
+                        'train' : 19,
+                        'tvmonitor' : 20}
+        
+        try:
+            w = x['annotation']['size']['width']
+            h = x['annotation']['size']['height']
+            
+            w = float(w)
+            h = float(h)
+
+            cnt = 0
+            anno = []
+            for obj in x['annotation']['object']:
+                xmax = float(obj['bndbox']['xmax']) / w
+                xmin = float(obj['bndbox']['xmin']) / w
+                ymax = float(obj['bndbox']['ymax']) / h
+                ymin = float(obj['bndbox']['ymin']) / h
+
+                anno.append([labels_map[obj['name']], xmax, xmin, ymax, ymin])
+                cnt += 1
+            
+            while cnt < 30:
+                anno.append([-1, -1, -1, -1, -1])
+                cnt += 1
+            
+            anno = torch.FloatTensor(anno)
+            print(anno.shape)
+            return anno
+        except:
+            print(x)
+
+
+
+
+
 
 
 if __name__ == '__main__':
     import os
     import json
     # print(os.getcwd())
-    data_loader = VocDataLoader("../data", 1)
+    data_loader = VocDataLoader("../data", 4, num_workers=4)
     for batch_idx, (data, target) in enumerate(data_loader):
+        target = VocDataLoader.anotations_tranform(target)
         # print(batch_idx, data.shape, target.shape)
-        print(batch_idx, data.shape, json.dumps(target, indent=4, sort_keys=True))
+        # print(batch_idx, data.shape, json.dumps(target, indent=4, sort_keys=True))
+        
         break
--- yolo/model/loss.py
+++ yolo/model/loss.py
@@ -3,3 +3,6 @@ import torch.nn.functional as F
 
 def nll_loss(output, target):
     return F.nll_loss(output, target)
+
+def yolov1_loss(output, target):
+    

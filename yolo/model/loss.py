bee5fecef7df449e1486a9a361dc3fc41734b533
ngocgiang99 <truongngocgiang99@gmail.com>
2021-05-28 15:29:20 +0700

    complete loss yolov1 loss function

--- yolo/model/loss.py
+++ yolo/model/loss.py
@@ -14,6 +14,8 @@ def yolov1_loss(output, target, hyper_param):
     lbd_coord = hyper_param['lbd_coord']
     lbd_noobj = hyper_param['lbd_noobj']
 
+    N = output.shape[0]
+
     one_obj_mask = target[:, :, :, 4] > 0
     no_obj_mask = target[:, :, :, 4] == 0
     one_obj_mask = one_obj_mask.unsqueeze(-1).expand_as(target)
@@ -26,9 +28,65 @@ def yolov1_loss(output, target, hyper_param):
     no_obj_loss = F.mse_loss(output_no_obj[:, no_obj_conf_idx], target_no_obj[:, no_obj_conf_idx], reduction='sum')
 
     # Coordinate loss and width-height loss
-    
+    # print(output[one_obj_mask].shape)
+    # print(target[one_obj_mask].shape)
+    output_obj = output[one_obj_mask].view(-1, 30)  # Get cell contains box from yolo output
+    target_obj = target[one_obj_mask].view(-1, 30)  # Get cell contains box from VOC annotation
+
+    pred_box = output_obj[:, :10].contiguous().view(-1, 5)          # Get [x,y,w,h,c] from output
+    target_box = target_obj[:, :10].contiguous().view(-1, 5)        #
+
+    # print(target_box.shape)
+    target_obj_iou = torch.FloatTensor(target_box.shape)
+
+    coo_response_obj = torch.zeros(target_box.shape, dtype=torch.bool)
+    print(coo_response_obj.shape)
+
+    for i in range(0, pred_box.shape[0], 2):
+        o_box = pred_box[i:i+2]
+        # print("obox shape: ", o_box.shape)
+        o_box_xywh = torch.zeros(o_box.shape)
+        o_box_xywh[:, 2:4] = o_box[:, 2:4]
+        o_box_xywh[:, 0:2] = o_box[:, 0:2] - 0.5 * o_box[:, 2:4]
+
+        t_box = target_box[i].view(-1, 5)
+        # print("t box shape: ", t_box.shape)
+        t_box_xywh = torch.zeros(t_box.shape)
+        t_box_xywh[:, 2:4] = t_box[:, 2:4]
+        t_box_xywh[:, 0:2] = t_box[:, 0:2] - 0.5 * t_box[:, 2:4]
+        iou = compute_iou(o_box_xywh, t_box_xywh)
+        # print(iou)
+        max_iou, max_index = iou.max(0)
+        # print(i, max_iou, max_index)
+
+        max_index = max_index.data
+        coo_response_obj[i+max_index] = True
+        target_obj_iou[i+max_index, 4] = max_iou.data
+
+    target_obj_iou = target_obj_iou.cuda()
+    # print(coo_response_obj)
+
+    # print(pred_box[coo_response_obj].shape)
+    output_pred = pred_box[coo_response_obj].view(-1, 5).cuda()
+    target_pred = target_box[coo_response_obj].view(-1, 5).cuda()
+    target_iou_pred = target_obj_iou[coo_response_obj].view(-1,5).cuda()
+
+    # print(output_pred.shape, target_pred.shape)
+    xy_loss = F.mse_loss(output_pred[:, 0:2], target_pred[:, 0:2], reduction='sum')
+    wh_loss = F.mse_loss(torch.sqrt(output_pred[:, 2:4]), torch.sqrt(target_pred[:, 2:4]), reduction='sum')
+    obj_conf_loss = F.mse_loss(output_pred[:, 4], target_iou_pred[:, 4], reduction='sum')
+    loc_loss = xy_loss + wh_loss
+
+    # Classes Loss
+    output_class = output[one_obj_mask][10:].cuda()
+    target_class = target[one_obj_mask][10:].cuda()
+    classes_loss = F.mse_loss(output_class, target_class, reduction='sum')
 
 
+
+    # Sum loss
+    loss = lbd_coord * loc_loss + obj_conf_loss + lbd_noobj * no_obj_loss + classes_loss
+    return loss / N
 
 
 def test_yolov1_loss():
--- yolo/utils/util.py
+++ yolo/utils/util.py
@@ -70,42 +70,54 @@ def compute_iou(out_yolo, voc_anno):
     bb1 = {}
     bb2 = {}
     
-    bb1['x1'] = out_yolo[0]
-    bb1['x2'] = out_yolo[0] + out_yolo[2]
-    bb1['y1'] = out_yolo[1]
-    bb1['y2'] = out_yolo[1] + out_yolo[3]
+    bb1['x1'] = out_yolo[:, 0]
+    bb1['x2'] = out_yolo[:, 0] + out_yolo[:, 2]
+    bb1['y1'] = out_yolo[:, 1]
+    bb1['y2'] = out_yolo[:, 1] + out_yolo[:, 3]
 
-    bb2['x1'] = voc_anno[0]
-    bb2['x2'] = voc_anno[1]
-    bb2['y1'] = voc_anno[2]
-    bb2['y2'] = voc_anno[3]
+    bb2['x1'] = voc_anno[:, 0]
+    bb2['x2'] = voc_anno[:, 0] + voc_anno[:, 2]
+    bb2['y1'] = voc_anno[:, 1]
+    bb2['y2'] = voc_anno[:, 1] + voc_anno[:, 3]
 
-    assert bb1['x1'] < bb1['x2']
-    assert bb1['y1'] < bb1['y2']
-    assert bb2['x1'] < bb2['x2']
-    assert bb2['y1'] < bb2['y2']
+    assert (bb1['x1'] < bb1['x2']).any()
+    assert (bb1['y1'] < bb1['y2']).any()
+    assert (bb2['x1'] < bb2['x2']).any()
+    assert (bb2['y1'] < bb2['y2']).any()
 
     # determine the coordinates of the intersection rectangle
-    x_left = max(bb1['x1'], bb2['x1'])
-    y_top = max(bb1['y1'], bb2['y1'])
-    x_right = min(bb1['x2'], bb2['x2'])
-    y_bottom = min(bb1['y2'], bb2['y2'])
+    x_left = torch.max(bb1['x1'], bb2['x1'])
+    y_top = torch.max(bb1['y1'], bb2['y1'])
+    x_right = torch.min(bb1['x2'], bb2['x2'])
+    y_bottom = torch.min(bb1['y2'], bb2['y2'])
 
-    if x_right < x_left or y_bottom < y_top:
-        return 0.0
+    # print(bb1)
+    # print(bb2)
+    # print(x_left)
+    # print(x_right)
+    # print(y_top)
+    # print(y_bottom)
+
+    
 
     # The intersection of two axis-aligned bounding boxes is always an
     # axis-aligned bounding box
     intersection_area = (x_right - x_left) * (y_bottom - y_top)
+    if (x_right < x_left).any() or (y_bottom < y_top).any():
+        intersection_area = torch.zeros(intersection_area.shape)
+    # print(intersection_area)
+    # print(intersection_area.shape)
 
     # compute the area of both AABBs
     bb1_area = (bb1['x2'] - bb1['x1']) * (bb1['y2'] - bb1['y1'])
     bb2_area = (bb2['x2'] - bb2['x1']) * (bb2['y2'] - bb2['y1'])
+    # print(bb1_area)
+    # print(bb2_area)
 
     # compute the intersection over union by taking the intersection
     # area and dividing it by the sum of prediction + ground-truth
     # areas - the interesection area
-    iou = intersection_area / float(bb1_area + bb2_area - intersection_area)
-    assert iou >= 0.0
-    assert iou <= 1.0
+    iou = intersection_area / (bb1_area + bb2_area - intersection_area)
+    assert (iou >= 0.0).any()
+    assert (iou <= 1.0).any()
     return iou

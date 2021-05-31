20fa6242809bf6078683f579d70ee8cbaebced11
ngocgiang99 <truongngocgiang99@gmail.com>
2021-05-31 17:52:59 +0700

    Add CIFAR10 data loader

--- efficient_net/data_loader/data_loaders.py
+++ efficient_net/data_loader/data_loaders.py
@@ -14,3 +14,25 @@ class MnistDataLoader(BaseDataLoader):
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class Cifar10DataLoader(BaseDataLoader):
+
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        self.img_size = (224, 224)
+        trsfm = transforms.Compose([
+            transforms.Resize(self.img_size),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        self.data_dir = data_dir
+        self.dataset = datasets.CIFAR10(self.data_dir, train=training, download=False, transform=trsfm)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+if __name__ == '__main__':
+    data_loader = Cifar10DataLoader("../data", 4, num_workers=4)
+
+    for batch_idx, (data, target) in enumerate(data_loader):
+        print(batch_idx, data.shape, target.shape)
+        print(target)
+        break

ecd5482afe9b65a1291d5e8a7a1fa6c89c8224f9
giangtn <truongngocgiang99@gmail.com>
2021-06-01 12:43:01 +0700

    Change config and data loader parameter

--- efficient_net/data_loader/data_loaders.py
+++ efficient_net/data_loader/data_loaders.py
@@ -26,7 +26,7 @@ class Cifar10DataLoader(BaseDataLoader):
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
         self.data_dir = data_dir
-        self.dataset = datasets.CIFAR10(self.data_dir, train=training, download=False, transform=trsfm)
+        self.dataset = datasets.CIFAR10(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
 if __name__ == '__main__':
